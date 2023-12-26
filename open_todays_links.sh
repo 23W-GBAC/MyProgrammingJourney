bc6bb3ba1725a1d20ebdf90aaa320f9e08bb355f
Juan Pablo Herrera Upegui <148862881+juanchoupegui@users.noreply.github.com>
2023-12-26 21:34:20 +0100

    "link0=" added to each day
    because it didn't work setting it at the beginning

--- open_todays_links.sh
+++ open_todays_links.sh
@@ -2,34 +2,37 @@
 
 # This script opens links of resources that I use each day. It is based on ECRI Schedule (HI-1), but manually made. Run 'chmod +x open_todays_links.sh' to make this file executable.
 
-link0="https://webmail.th-deg.de/4e4MFLGEbtoejgFf/?_task=mail&_mbox=INBOX"
-
 # Get the current day of the week
 day=$(date +%A)
 
 # Define links based on the day
 case $day in
 	"Monday")
+ 		link0="https://webmail.th-deg.de/4e4MFLGEbtoejgFf/?_task=mail&_mbox=INBOX"
 		link1="https://ilearn.th-deg.de/course/view.php?id=17309"
 		link2=""
 		link3=""
 		;;
 	"Tuesday")
+ 		link0="https://webmail.th-deg.de/4e4MFLGEbtoejgFf/?_task=mail&_mbox=INBOX"
 		link1="https://teams.microsoft.com/_#/conversations/General%20Business%20Administration%20and%20Accounting?threadId=19:4385ddd1b9b840e0a959c2e113026483@thread.tacv2&ctx=channel"
 		link2="https://ilearn.th-deg.de/course/view.php?id=17809"
 		link3="https://github.com/dashboard"
 		;;
 	"Wednesday")
+ 		link0="https://webmail.th-deg.de/4e4MFLGEbtoejgFf/?_task=mail&_mbox=INBOX"
 		link1="https://ilearn.th-deg.de/course/view.php?id=17310"
 		link2=""
 		link3=""
 		;;
 	"Thursday")
+ 		link0="https://webmail.th-deg.de/4e4MFLGEbtoejgFf/?_task=mail&_mbox=INBOX"
 		link1="https://ilearn.th-deg.de/course/view.php?id=18236"
 		link2="https://ilearn.th-deg.de/course/view.php?id=17312"
 		link3="https://jupyter.org/try-jupyter/lab/?path=notebooks%2FIntro.ipynb"
 		;;
 	"Friday")
+ 		link0="https://webmail.th-deg.de/4e4MFLGEbtoejgFf/?_task=mail&_mbox=INBOX"
 		link1="https://ilearn.th-deg.de/course/view.php?id=17809"
 		link2=""
 		link3=""
